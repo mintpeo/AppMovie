1b6f9ff865aa6d54341e5a31bcc9760493dc8a9f
Bảo Trân <19130241@st.hcmuaf.edu.vn>
2024-06-18 15:54:40 +0700

    code admin
    admin

--- app/src/main/java/com/example/myappmovielastup/activity/ProductAdapter.java
+++ app/src/main/java/com/example/myappmovielastup/activity/ProductAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,20 +14,35 @@ import com.example.myappmovielastup.R;
 
 import java.util.ArrayList;
 
-public class ProductAdapter {
+public class ProductAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Product> productList;
 
     public ProductAdapter(Context context, ArrayList<Product> productList) {
-        super();
         this.context = context;
         this.productList = productList;
     }
 
+    @Override
+    public int getCount() {
+        return productList.size();
+    }
+
+    @Override
+    public Product getItem(int position) {
+        return productList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItemView = convertView;
         if (listItemView == null) {
-            listItemView = LayoutInflater.from(parent.getContext()).inflate(
+            listItemView = LayoutInflater.from(context).inflate(
                     R.layout.list_item_product, parent, false);
         }
 
@@ -66,10 +82,8 @@ public class ProductAdapter {
         return listItemView;
     }
 
-    private void notifyDataSetChanged() {
-    }
-
-    private Product getItem(int position) {
-        return null;
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
     }
 }
